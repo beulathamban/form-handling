8cb82d4cb23bc116fd93a0616def354f2fd006e2
beulathamban <35990861+beulathamban@users.noreply.github.com>
2018-04-10 11:54:47 +1200

    store user input in textbox

--- add.php
+++ add.php
@@ -9,7 +9,8 @@ $n=$_POST["text1"];
 ?> 
 <body>
 	<h1> Yoobee's calculator </h1>
-	<form action="" method="post id="option">
+	<!-- when allowing files to be uploaded, include enctype attribute in the form tag -->
+	<form action="adding.php" method="post" enctype="multipart/form-data" id="option">
 	Circle <input type="radio" name="circle" value="circle" checked="checked">
 	Square <input type="radio" name="circle" value="square">
 	<input type="submit" name="submit" value="proceed"> 
@@ -17,9 +18,11 @@ $n=$_POST["text1"];
 	<hr>
 <form action="" method="post" id="circle">
  Enter the radius : <input type="text" name="text1" value="<?php echo $n;?>">
+ <!-- receiving user data in the form and using it to display a calculated value in the same form -->
 Diameter : <input type="text" name="dia" value="<?php echo 2*$_POST[text1]; ?>" >
 <input type="submit" name="submit" value="display"> 
-<!-- <input type = "text" name = "text3" value="<?php echo $sum; ?>" > -->
+<!-- File uploading using form . PHP engine creates a $_FILES superglobal variable to receive files-->
+<input type="file" name="photo" id="photo" value="" />
 </form>
 </body>
  
